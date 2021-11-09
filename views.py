5228db31ff8b82296356def8dc1eed03c6116197
Dharshsun <62539407+Dharshsun@users.noreply.github.com>
2021-11-09 12:37:29 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def index(request):
+   meetups = [
+      { 
+         'title':'A first meetup',
+         'location':'New york',
+         'slug':'a-first meetup'
+      },
+       { 
+      
+         'title': 'A Second Meetup',
+         'location':'Paris',
+         'slug':'a-second-meetup'
+       }
+   ]
+   return render(request, 'meetups/index.html',{
+      'show_meetups':True,
+      'meetups':meetups
+   })
